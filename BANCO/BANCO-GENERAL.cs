c4973c3c6543fec2075d6a7a8b561f2f492413f7
Guillermo141 <paredesguillermo949@gmail.com>
2024-03-05 15:47:28 -0600

    nada

---
+++ BANCO/BANCO-GENERAL.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BANCO
+{
+    internal class BANCO_GENERAL
+    {
+            private int Num_Cuenta;
+            private int Saldo;
+            private string Nombre;
+
+            public BANCO_GENERAL(int numCuenta, int saldo, string nombre)
+            {
+                Num_Cuenta = numCuenta;
+                Saldo = saldo;
+                Nombre = nombre;
+            }
+
+            public void Operacion()
+            {
+                Console.WriteLine("Que operacion deseas realizar");
+                Console.WriteLine("1(Retirar)");
+                Console.WriteLine("2(Depositar)");
+                Console.WriteLine("3(Consultar saldo)");
+                int opcion = 0;
+                Console.WriteLine(" ");
+                opcion = int.Parse(Console.ReadLine());
+
+                int saldo = Saldo;
+                int reti = 0, resta = 0, depo = 0, suma = 0;
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("Que cantidad desea retirar");
+                        reti = int.Parse(Console.ReadLine()); 
+                        resta = saldo - reti;
+                        Console.WriteLine("Su saldo ahora es: " + resta);
+                        break;
+                    case 2:
+                        Console.WriteLine("De cuanto va ser su deposito");
+                        depo = int.Parse(Console.ReadLine());
+                        suma = saldo + depo;
+                        Console.WriteLine("Su saldo aumento a: " + suma);
+                        break;
+                    case 3:
+                        Console.WriteLine("Desea consuktar su saldo");
+                        Console.WriteLine("Su saldo es de " + saldo);
+
+                        break;
+                    default:
+                        Console.WriteLine("Esa operacion no existe!");
+                        break;
+                }
+
+            }
+        }
+    }
---
+++ BANCO/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BANCO;
+
+
+Console.WriteLine("Buenos dias somos el banco BBVA");
+Console.WriteLine("El banco fuerte de mexico");
+Console.WriteLine("TLAXCALA,MEXICO");
+BANCO_GENERAL cuenta = new BANCO_GENERAL(2254, 500, "Guillermosp");
+
+cuenta.Operacion();
